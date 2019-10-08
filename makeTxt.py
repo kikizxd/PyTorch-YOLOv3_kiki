cee79b66c57fb1da7c09d0afb3a24a535048c7c2
kikizxd <453079636@qq.com>
2019-10-08 13:32:31 +0800

    first upload

---
+++ makeTxt.py
@@ -0,0 +1,31 @@
+import os
+import random
+trainval_percent = 0.1
+train_percent = 0.9
+xmlfilepath = 'data/custom/Annotations'
+txtsavepath = 'data/custom/ImageSets'
+total_xml = os.listdir(xmlfilepath)
+num = len(total_xml)
+list = range(num)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+trainval = random.sample(list, tv)
+train = random.sample(trainval, tr)
+ftrainval = open('data/custom/ImageSets/trainval.txt', 'w')
+ftest = open('data/custom/ImageSets/test.txt', 'w')
+ftrain = open('data/custom/ImageSets/train.txt', 'w')
+fval = open('data/custom/ImageSets/valid.txt', 'w')
+for i in list:
+    name = total_xml[i][:-4] + '\n'
+    if i in trainval:
+        ftrainval.write(name)
+        if i in train:
+            ftest.write(name)
+        else:
+            fval.write(name)
+    else:
+        ftrain.write(name)
+ftrainval.close()
+ftrain.close()
+fval.close()
+ftest.close()
---
+++ train.py
@@ -0,0 +1,203 @@
+from __future__ import division
+
+from models import *
+from utils.logger import *
+from utils.utils import *
+from utils.datasets import *
+from utils.parse_config import *
+from test import evaluate
+
+from terminaltables import AsciiTable
+
+import os
+import sys
+import time
+import datetime
+import argparse
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from torch.autograd import Variable
+import torch.optim as optim
+
+
+
+if __name__ == "__main__":
+    #argparse用于解析命令行参数
+	 #argparse使用步骤：
+    #0、导入模块
+    #import argparse
+	 #1、创建解析对象
+    parser = argparse.ArgumentParser()
+	 #2、向对象中添加命令行参数和选项
+	 ##type为参数类型，默认字符串string，default为默认值
+    parser.add_argument("--epochs", type=int, default=20, help="number of epochs")#epochs=100
+    parser.add_argument("--batch_size", type=int, default=4, help="size of each image batch")
+    parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
+    parser.add_argument("--model_def", type=str, default="config/yolov3.cfg", help="path to model definition file")
+    parser.add_argument("--data_config", type=str, default="config/coco.data", help="path to data config file")
+    parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+    parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model weights")
+    parser.add_argument("--evaluation_interval", type=int, default=1, help="interval evaluations on validation set")
+    parser.add_argument("--compute_map", default=False, help="if True computes mAP every tenth batch")
+    parser.add_argument("--multiscale_training", default=True, help="allow for multi-scale training")
+    #3、调用parse_args()方法进行解析
+    opt = parser.parse_args()
+    print(opt)
+		
+	 #pytorch没有自己的tensorboard，调用utils_all.py中的类，使pytorch可以调用tensorboard
+    logger = Logger("logs")
+	
+	 #设备使用，如果有cuda，用cuda，没有的话在cpu上运行
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	
+	 #创建多级目录
+	 #exist_ok默认False，即要创建的目录已经存在的话，会报OSError错
+    os.makedirs("output", exist_ok=True)
+    os.makedirs("checkpoints", exist_ok=True)
+
+    #获取数据配置
+	 #parse_data_config是utils_all中的函数，将数据传给不同的gpu设备
+    data_config = parse_data_config(opt.data_config)
+    train_path = data_config["train"]
+    valid_path = data_config["valid"]
+	 #load_classes是utils_all种的函数，加载路径
+    class_names = load_classes(data_config["names"])
+
+    #模型启动
+	 ##Darknet是models.py中定义的YoLo-v3目标检测模型
+	 ##opt.model_def是设置以上opt参数后的YoLo-v3模型
+	 ##weights_init_normal在utils_all.py中有定义这个函数，设置初始权重
+    model = Darknet(opt.model_def).to(device)
+    model.apply(weights_init_normal)
+	
+	 #权重加载
+    #如果已经存在预训练权重，直接加载；如果不存在，训练构建后加载
+    if opt.pretrained_weights:
+        if opt.pretrained_weights.endswith(".pth"):
+            model.load_state_dict(torch.load(opt.pretrained_weights))
+        else:
+            model.load_darknet_weights(opt.pretrained_weights)
+
+    #数据加载
+	 ##ListDataset是utils_all中的类，对数据集简单操作，使其满足input输入格式（包括分成一个个batch）
+    dataset = ListDataset(train_path, augment=True, multiscale=opt.multiscale_training)
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=opt.batch_size,
+        shuffle=True,
+        num_workers=opt.n_cpu,
+        pin_memory=True,
+        collate_fn=dataset.collate_fn,
+    )
+
+	 #优化器的选择
+    optimizer = torch.optim.Adam(model.parameters())
+
+	 #
+    metrics = [
+        "grid_size",
+        "loss",
+        "x",
+        "y",
+        "w",
+        "h",
+        "conf",
+        "cls",
+        "cls_acc",
+        "recall50",
+        "recall75",
+        "precision",
+        "conf_obj",
+        "conf_noobj",
+    ]
+	
+	 #epoch迭代
+	 ##one epoch = numbers of iterations = 训练样本的数量/batch_size
+    for epoch in range(opt.epochs):
+        model.train()
+        start_time = time.time()
+        for batch_i, (_, imgs, targets) in enumerate(dataloader):
+            batches_done = len(dataloader) * epoch + batch_i
+
+            imgs = Variable(imgs.to(device))
+            targets = Variable(targets.to(device), requires_grad=False)
+
+            loss, outputs = model(imgs, targets)
+            loss.backward()
+
+            if batches_done % opt.gradient_accumulations:
+                # Accumulates gradient before each step
+                optimizer.step()
+                optimizer.zero_grad()
+
+            # ----------------
+            #   Log progress
+            # ----------------
+
+            log_str = "\n---- [Epoch %d/%d, Batch %d/%d] ----\n" % (epoch, opt.epochs, batch_i, len(dataloader))
+
+            metric_table = [["Metrics", *[f"YOLO Layer {i}" for i in range(len(model.yolo_layers))]]]
+
+            # Log metrics at each YOLO layer
+            for i, metric in enumerate(metrics):
+                formats = {m: "%.6f" for m in metrics}
+                formats["grid_size"] = "%2d"
+                formats["cls_acc"] = "%.2f%%"
+                row_metrics = [formats[metric] % yolo.metrics.get(metric, 0) for yolo in model.yolo_layers]
+                metric_table += [[metric, *row_metrics]]
+
+                # Tensorboard logging
+                tensorboard_log = []
+                for j, yolo in enumerate(model.yolo_layers):
+                    for name, metric in yolo.metrics.items():
+                        if name != "grid_size":
+                            tensorboard_log += [(f"{name}_{j+1}", metric)]
+                tensorboard_log += [("loss", loss.item())]
+                logger.list_of_scalars_summary(tensorboard_log, batches_done)
+
+            log_str += AsciiTable(metric_table).table
+            log_str += f"\nTotal loss {loss.item()}"
+
+            # Determine approximate time left for epoch
+            epoch_batches_left = len(dataloader) - (batch_i + 1)
+            time_left = datetime.timedelta(seconds=epoch_batches_left * (time.time() - start_time) / (batch_i + 1))
+            log_str += f"\n---- ETA {time_left}"
+
+            print(log_str)
+
+            model.seen += imgs.size(0)
+
+        if epoch % opt.evaluation_interval == 1:
+            print("\n---- Evaluating Model ----")
+            # Evaluate the model on the validation set
+            precision, recall, AP, f1, ap_class = evaluate(
+                model,
+                path=valid_path,
+                iou_thres=0.5,
+                conf_thres=0.5,
+                nms_thres=0.5,
+                img_size=opt.img_size,
+                batch_size=4,
+            )
+            evaluation_metrics = [
+                ("val_precision", precision.mean()),
+                ("val_recall", recall.mean()),
+                ("val_mAP", AP.mean()),
+                ("val_f1", f1.mean()),
+            ]
+            logger.list_of_scalars_summary(evaluation_metrics, epoch)
+
+            # Print class APs and mAP
+            ap_table = [["Index", "Class name", "AP"]]
+            for i, c in enumerate(ap_class):
+                ap_table += [[c, class_names[c], "%.5f" % AP[i]]]
+            print(AsciiTable(ap_table).table)
+            print(f"---- mAP {AP.mean()}")
+
+        if epoch % opt.checkpoint_interval == 0:
+            torch.save(model.state_dict(), f"checkpoints/yolov3_ckpt_%d.pth" % epoch)
